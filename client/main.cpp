03874a79aeeaddcb5f193e9fd23808e7dd075e54
vfjpl <vfjpl1@gmail.com>
2018-04-11 17:46:43 +0200

    basic network

--- client/main.cpp
+++ client/main.cpp
@@ -9,20 +9,20 @@
 void print_help()
 {
     std::cout<<"-p --port       default 7000\n"
-               "-l --local_port default 8000\n"
-               "-i --ip         default localhost\n"
-               "-h --help       this message\n";
+             "-l --local_port default 8000\n"
+             "-i --ip         default localhost\n"
+             "-h --help       this message\n";
 }
 
 int main(int argc, char** argv)
 {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //zmienne które można modyfikować argumentami z konsoli
-    unsigned short port = 8000;
+    unsigned short local_port = 8000;
     unsigned short remote_port = 7000;//port do którego się łączymy
     sf::IpAddress remote_ip = "localhost";//ip do którego się łączymy
 
-    #ifdef linux
+#ifdef linux
     {
         static struct option longopts[] =
         {
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
                 remote_port = std::stoul( optarg, NULL, 0 );
                 break;
             case 'l':
-                port = std::stoul( optarg, NULL, 0 );
+                local_port = std::stoul( optarg, NULL, 0 );
                 break;
             case 'i':
                 remote_ip = optarg;
@@ -57,137 +57,149 @@ int main(int argc, char** argv)
             }
         }
     }
-    #endif // linux
+#endif // linux
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    bool quit;
+    bool quit = false;
     sf::UdpSocket socket;
-    socket.bind( port );
+    socket.setBlocking( false );
+    socket.bind( local_port );
+
 
     sf::Packet send_packet;
     sf::Packet receive_packet;
     sf::IpAddress incomming_ip;
+    unsigned short incomming_port;
 
-	int map_width = 32, map_height = 32;
-	int number_of_chunks = map_width * map_height;
+    int map_width = 32, map_height = 32;
+    int number_of_chunks = map_width * map_height;
 
-	sf::RenderWindow oknoAplikacji(sf::VideoMode(1920, 1080), "Kelajno", sf::Style::Fullscreen);//to opcja fullscreen
+    sf::RenderWindow oknoAplikacji(sf::VideoMode(1920, 1080), "Kelajno", sf::Style::Fullscreen);//to opcja fullscreen
 
-	sf::Texture tekstura1;//sluzy do wczytywania tekstury bo jest texture i image
-	tekstura1.loadFromFile("Textures/Grunt.png");//zwraca true lub false
-	sf::Texture tekstura2;
-	tekstura2.loadFromFile("Textures/Grunt2.png");
-	sf::Texture tekstura3;
-	tekstura3.loadFromFile("Textures/Drzewko.png");
-
-
-	sf::Sprite *obrazek = new sf::Sprite[number_of_chunks];
-	for (int i = 0; i < number_of_chunks; i++)
-	{
-		if (i % 2 == 0)
-		{
-			obrazek[i].setTexture(tekstura1);
-		}
-		else
-		{
-			obrazek[i].setTexture(tekstura2);
-		}
-	}
-	sf::Sprite drzewo;
-	drzewo.setTexture(tekstura3);
+    sf::Texture tekstura1;//sluzy do wczytywania tekstury bo jest texture i image
+    tekstura1.loadFromFile("Textures/Grunt.png");//zwraca true lub false
+    sf::Texture tekstura2;
+    tekstura2.loadFromFile("Textures/Grunt2.png");
+    sf::Texture tekstura3;
+    tekstura3.loadFromFile("Textures/Drzewko.png");
 
 
-	sf::FloatRect chunk_size = obrazek[0].getGlobalBounds();//biore info wymiarow chunka na przykladzie pierwszego
-	for (int i = 0; i < map_height; i++)
-	{
-		for (int g = 0; g < map_width; g++)
-		{
-			obrazek[g + (i*map_width)].setPosition( (oknoAplikacji.getSize().x * 0.5+250) - (g*chunk_size.width),
-                                                    (oknoAplikacji.getSize().y * 0.5-250) + (i*chunk_size.height) );
-			//podwojna petla "i" dla nr wiersza i "g" dla kolumny
-			//do szerokosci dodaje wielokrotnosci kolumnowe i wierszowe dla wysokosci
-			//250 na korekte
-		}
-	}
-	drzewo.setPosition((oknoAplikacji.getSize().x * 0.5), (oknoAplikacji.getSize().y * 0.5));
-
-	sf::View v = oknoAplikacji.getDefaultView();//widok ma byc taki jak okno tak jakby ciagnie z niego dane
-	v.setSize(v.getSize().x, v.getSize().y * 2);//tak jak przy teksturze skalujemy 2 wieksza wysoksoc
-	v.setRotation(45);
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    sf::Thread network_thread( [&]()
+    sf::Sprite *obrazek = new sf::Sprite[number_of_chunks];
+    for (int i = 0; i < number_of_chunks; i++)
+    {
+        if (i % 2 == 0)
         {
-            while( !quit )
-            {
-                socket.receive( receive_packet, incomming_ip, port );
-            }
-        });
+            obrazek[i].setTexture(tekstura1);
+        }
+        else
+        {
+            obrazek[i].setTexture(tekstura2);
+        }
+    }
+    sf::Sprite drzewo;
+    drzewo.setTexture(tekstura3);
+
+
+    sf::FloatRect chunk_size = obrazek[0].getGlobalBounds();//biore info wymiarow chunka na przykladzie pierwszego
+    for (int i = 0; i < map_height; i++)
+    {
+        for (int g = 0; g < map_width; g++)
+        {
+            obrazek[g + (i*map_width)].setPosition( (oknoAplikacji.getSize().x*0.5 + 250) - (g*chunk_size.width),
+                                                    (oknoAplikacji.getSize().y*0.5 - 250) + (i*chunk_size.height) );
+            //podwojna petla "i" dla nr wiersza i "g" dla kolumny
+            //do szerokosci dodaje wielokrotnosci kolumnowe i wierszowe dla wysokosci
+            //250 na korekte
+        }
+    }
+    drzewo.setPosition((oknoAplikacji.getSize().x * 0.5), (oknoAplikacji.getSize().y * 0.5));
+
+    sf::View v = oknoAplikacji.getDefaultView();//widok ma byc taki jak okno tak jakby ciagnie z niego dane
+    v.setSize(v.getSize().x, v.getSize().y * 2);//tak jak przy teksturze skalujemy 2 wieksza wysoksoc
+    v.setRotation(45);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     sf::Time time;
     sf::Clock clock;
 
     //tymczasowe
     //wysylanie prosby o dolaczenie do gry
-    send_packet << (sf::Uint8)0;//command
+    send_packet << (sf::Uint8)1;//command
     socket.send( send_packet, remote_ip, remote_port );
     send_packet.clear();
 
-	while ( !quit )
-	{
+    while ( !quit )
+    {
         time = clock.restart();//pobranie czasu
 
+
+        //odbieranie pakietów
+        while ( !socket.receive( receive_packet, incomming_ip, incomming_port) )
+        {
+            while( !receive_packet.endOfPacket() )
+            {
+                sf::Uint8 command;
+                receive_packet >> command;
+
+                switch( command )
+                {
+                default:
+                    break;
+                }
+            }
+
+        }
 
         //obsluga urządzeń wejścia
         sf::Event zdarzenie;
         while( oknoAplikacji.pollEvent(zdarzenie) )
         {
             switch (zdarzenie.type)
-            	{
-            	case sf::Event::Closed:
-                    quit = true;
-                    break;
-            	default:
-                    break;
-            	}
+            {
+            case sf::Event::Closed:
+                quit = true;
+                break;
+            default:
+                break;
+            }
 
             switch(zdarzenie.key.code)
-                {
-            	case sf::Keyboard::Escape:
-                    quit = true;
-                    break;
-            	default:
-                    break;
-                }
+            {
+            case sf::Keyboard::Escape:
+                quit = true;
+                break;
+            default:
+                break;
+            }
 
             switch(zdarzenie.mouseButton.button)
-                {
-                case sf::Mouse::Middle:
-                    quit = true;
-                    break;
-            	default:
-                    break;
-            	}
+            {
+            case sf::Mouse::Middle:
+                quit = true;
+                break;
+            default:
+                break;
+            }
         }
 
 
         //rysowanie
-		oknoAplikacji.clear();
-		oknoAplikacji.setView(v);//ustawia widok
-		for (int i = 0; i < number_of_chunks; i++)
-		{
-			oknoAplikacji.draw(obrazek[i]);//wyswietla chunki
-		}
-		oknoAplikacji.setView(oknoAplikacji.getDefaultView());//zeby zrzutowac drzewo prosto
-		oknoAplikacji.draw(drzewo);
-		oknoAplikacji.display();
+        oknoAplikacji.clear();
+        oknoAplikacji.setView(v);//ustawia widok
+        for (int i = 0; i < number_of_chunks; i++)
+        {
+            oknoAplikacji.draw(obrazek[i]);//wyswietla chunki
+        }
+        oknoAplikacji.setView(oknoAplikacji.getDefaultView());//zeby zrzutowac drzewo prosto
+        oknoAplikacji.draw(drzewo);
+        oknoAplikacji.display();
 
 
 
-		socket.send( send_packet, remote_ip, remote_port);//wyslanie pakietu
-		send_packet.clear();//czyszczenie pakietu
-	}
+        socket.send( send_packet, remote_ip, remote_port);//wyslanie pakietu
+        send_packet.clear();//czyszczenie pakietu
+    }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     delete[] obrazek;
-	oknoAplikacji.close();
+    oknoAplikacji.close();
 
     return EXIT_SUCCESS;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,31 +1,23 @@
 #include <SFML/Network.hpp>
 #include <iostream>
-#include <unordered_map>
 
-/*
-#ifdef _WIN32
-    #include <cstring>
-#endif // _WIN32
-*/
 #ifdef linux
     #include <getopt.h>
 #endif // linux
 
-#include "../common/player.hpp"
-
 void print_help()
 {
     std::cout<<"-p  --port      default 7000\n"
-               "-h  --help      this message\n";
+             "-h  --help      this message\n";
 }
 
 int main(int argc, char** argv)
 {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //zmienne które można modyfikować argumentami z konsoli
-    unsigned short port = 7000;
+    unsigned short local_port = 7000;
 
-    #ifdef linux
+#ifdef linux
     {
         static struct option longopts[] =
         {
@@ -39,7 +31,7 @@ int main(int argc, char** argv)
             switch (c)
             {
             case 'p':
-                port = std::stoul( optarg, NULL, 0 );
+                local_port = std::stoul( optarg, NULL, 0 );
                 break;
             case 'h':
                 std::cout<<argv[0]<<"\n";
@@ -52,49 +44,43 @@ int main(int argc, char** argv)
             }
         }
     }
-    #endif // linux
+#endif // linux
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    bool quit;
+    bool quit = false;
     sf::UdpSocket socket;
-    socket.bind( port );
+    socket.bind( local_port );
 
     sf::Packet send_packet;
     sf::Packet receive_packet;
     sf::IpAddress incomming_ip;
+    unsigned short incomming_port;
 
-    std::unordered_map <std::string, player> players;
-    //std::vector <unit> units;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     sf::Time time;
     sf::Clock clock;
     while( !quit )
     {
-        socket.receive( receive_packet, incomming_ip, port );
+        socket.receive( receive_packet, incomming_ip, incomming_port );
         time = clock.restart();
 
         //obsługa pakietu
-        sf::Int64 usertime;
-        receive_packet >> usertime;//odebranie czasu z pakietu
-        send_packet << usertime;
-        while(!receive_packet.endOfPacket())
+        while( !receive_packet.endOfPacket() )
         {
             sf::Uint8 command;
             receive_packet >> command;
 
             switch( command )
             {
-            case 0:
-                players.insert( {incomming_ip.toString() + std::to_string(port), player(incomming_ip,port)} );
+            case 1:
+                std::cout<<"New Player!\n";
                 break;
-            //od 0 do 9 zarezerwowane
-
             default:
                 break;
             }
         }
 
 
-        socket.send( send_packet, incomming_ip, port );
+        socket.send( send_packet, incomming_ip, incomming_port );
         send_packet.clear();
     }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
